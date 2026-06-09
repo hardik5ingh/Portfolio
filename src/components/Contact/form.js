883098d2474e18411497d7649218bc07417cbf88
Hardik <hardikmaurya1502@gmail.com>
2026-06-09 20:03:50 +0530

    changes

--- src/components/Contact/form.js
+++ src/components/Contact/form.js
@@ -10,14 +10,14 @@ const app = express();
 app.use(cors({ origin: 'https://hardik5ingh.netlify.app/' }));
 app.use(express.json());
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
-
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD || "",
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT
+// });
+ const db = mysql.createConnection(process.env.MYSQL_URL);
 const auth = new google.auth.GoogleAuth({
   keyFile: "credentials.json",
   scopes: ["https://www.googleapis.com/auth/spreadsheets"]
